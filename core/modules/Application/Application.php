6dde9a3e1624d2e594625f209e36d99744b9aea6
Ray <rbrowell73@gmail.com>
2016-11-11 17:32:05 -0600

    php version
    new repo

---
+++ core/modules/Application/Application.php
@@ -0,0 +1,103 @@
+<?php
+/**
+* Staff Application v.1.0 Module
+*
+* phpVMS Module for pilots to submit a staff application sent via email
+* This module is released under the Creative Commons Attribution-Noncommercial-Share Alike 3.0 Unported License
+* You are free to redistribute and alter this work as you wish but you must keep the original 'copyright' information on all the places it comes in the original work.
+* You are not allowed to delete the copyright information and/or gain any profit by adopting or using this module.
+*
+* @author Chase Morgan
+* @copyright Copyright (c) 2013, Chase Morgan
+* @license http://creativecommons.org/licenses/by-nc-sa/3.0/
+*/
+
+
+class Application extends CodonModule 
+{
+	public function index()
+	{
+		if(!Auth::LoggedIn())
+		{
+			$this->set('message', 'You must be logged in to access this feature!');
+			$this->render('core_error.php');
+			return;
+		}
+                //Google reCaptcha
+                //updated to Google noCaptcha 1/15
+		require_once CORE_LIB_PATH.'/recaptcha/recaptchalib.php';
+
+                $this->set('sitekey', RECAPTCHA_PUBLIC_KEY);
+                $this->set('lang', 'en');
+
+
+		if($this->post->submit)	{
+			if(Auth::LoggedIn() == false) {
+				# Make sure they entered an email address
+				if(trim($this->post->name) == '' || trim($this->post->email) == '') {
+					$this->set('message', 'You must enter a name and email!');
+					$this->render('core_error.tpl');
+					return;
+				}
+			}
+
+                        //Google reCaptcha
+                        //updated to Google noCaptcha 1/15
+                        $resp = null;
+                        $reCaptcha = new ReCaptcha(RECAPTCHA_PRIVATE_KEY);
+                        // Was there a reCAPTCHA response?
+                        if ($_POST["g-recaptcha-response"]) {
+                                $resp = $reCaptcha->verifyResponse(
+                                $_SERVER["REMOTE_ADDR"],
+                                $_POST["g-recaptcha-response"]
+                            );
+                        }
+
+                        //check if reCaptcha response was valid
+                        if ($resp == null) {
+                            $this->set('captcha_error', 'reCaptcha Validation Error');
+                            $this->render('application_form.php');
+                            return;
+                        }
+                        //end Google reCaptcha
+			
+			if($this->post->subject == '' || trim($this->post->message) == '')
+			
+			
+			$subject = 'New message from '.$this->post->name.' - "'.$this->post->subject.'"';
+			$message = DB::escape($this->post->message) . PHP_EOL . PHP_EOL;
+			
+			unset($_POST['recaptcha_challenge_field']);
+			unset($_POST['recaptcha_response_field']);
+			
+			foreach($_POST as $field=>$value)
+			{
+				$message.="-$field = $value".PHP_EOL;
+			}
+			
+			$message = nl2br($message);
+			$message = utf8_encode($message);
+			Util::SendEmail(ADMIN_EMAIL, $subject, $message);
+			
+			$this->render('application_sent.php');
+			return;
+		}		
+		
+		# Just a simple addition
+		$rand1 = rand(1, 10);
+		$rand2 = rand(1, 10);
+		
+		$this->set('rand1', $rand1);
+		$this->set('rand2', $rand2);		
+		
+		$tot = $rand1 + $rand2;
+		//echo "total: $tot <br />";
+		SessionManager::Set('captcha_sum', $tot);
+		
+		//echo 'output of $_SESSION: <br />';
+		//print_r($_SESSION);
+		
+		$this->render('application_form.php');
+	}
+	
+}
---
+++ core/templates/application/application_form.php
@@ -0,0 +1,101 @@
+<?php if(!defined('IN_PHPVMS') && IN_PHPVMS !== true) { die(); } ?>
+<p>
+<h2>Staff Application</h2>
+</p>
+<p>
+Thank you for your interest in a staff position at <?php echo SITE_NAME; ?>. Please complete the following form and fill out all fields. Our careers team will get back to you within 48 hours with a responce.
+<hr />
+ 
+</p>
+<p>
+<form method="post" action="<?php echo url('/application'); ?>">
+
+  <table width='100%' border='0'>
+    <tr>
+      <td><strong>Name: *</strong></td>
+      <td>
+		<?php
+		if(Auth::LoggedIn())
+		{
+			echo Auth::$userinfo->firstname .' '.Auth::$userinfo->lastname;
+			echo '<input type="hidden" name="name" 
+					value="'.Auth::$userinfo->firstname 
+							.' '.Auth::$userinfo->lastname.'" />';
+		}
+		else
+		{
+		?>
+		<?php
+		}
+		?>
+      </td>
+    </tr>
+    <tr>
+		<td width="1%" nowrap><strong>Pilot ID: *</strong></td>
+		<td>
+		<?php
+		if(Auth::LoggedIn())
+		{
+			echo Auth::$userinfo->pilotid;
+			echo '<input type="hidden" name="name" 
+					value="'.Auth::$userinfo->pilotid.'" />';
+		}
+		else
+		{
+		?>
+		<?php
+		}
+		?>
+		</td>
+	</tr>
+
+	<tr>
+		<td><strong>Position: *</strong></td>
+	<td>
+		<select>
+		<option value="Chief Executive Officer">Chief Executive Officer</option>
+  		<option value="Cheif Operations Officer">Cheif Operations Officer</option>
+ 		<option value="Human Resources">Human Resources</option>
+  		<option value="Flight Dispatcher">Flight Dispatcher</option>
+  		<option value="Webmaster">Webmaster</option>
+		</select></td>
+	</tr>
+
+	<tr>
+		<td><strong>Tell Us About Your Previous VA Experiance: *</strong></td>
+		<td><textarea name="message" cols='45' rows='5'><?php echo $_POST['message'];?></textarea></td>
+	
+	</tr>
+    <tr>
+      <td><strong>Why Should We Choose You For This Position?: *</strong></td>
+      <td>
+		<textarea name="message" cols='45' rows='5'><?php echo $_POST['message'];?></textarea>
+      </td>
+    </tr>
+    <tr>
+      <td><strong>Do You Have Any Real World Experiance?: (Optional)</strong></td>
+      <td>
+		<textarea name="message" cols='45' rows='5'><?php echo $_POST['message'];?></textarea>
+      </td>
+    </tr>
+    
+    <tr>
+		<td width="1%" nowrap><strong>Captcha</strong></td>
+		<td>
+		<?php if(isset($captcha_error)){echo '<p class="error">'.$captcha_error.'</p>';} ?>
+        <div class="g-recaptcha" data-sitekey="<?php echo $sitekey;?>"></div>
+        <script type="text/javascript" src="https://www.google.com/recaptcha/api.js?hl=<?php echo $lang;?>">
+        </script>
+		</td>
+	</tr>
+	
+    <tr>
+		<td>
+			<input type="hidden" name="loggedin" value="<?php echo (Auth::LoggedIn())?'true':'false'?>" />
+		</td>
+		<td>
+          <input type="submit" name="submit" value='Send Message'>
+		</td>
+    </tr>
+  </table>
+</form>
