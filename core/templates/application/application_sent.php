f65adb1960cce2d309a8b4dfa3cc407ed010ca00
Ray <rbrowell73@gmail.com>
2016-11-11 20:44:14 -0600

    Success Redirect
    Redirect from success page

--- core/templates/application/application_sent.php
+++ core/templates/application/application_sent.php
@@ -1,3 +1,7 @@
+<!DOCTYPE HTML>
+ 
+<meta charset="UTF-8">
+<meta http-equiv="refresh" content="10; url=../index.php">
 <p>
 <section class="page-contents">
 <div class="container">
@@ -6,6 +10,9 @@
 <p>
 <div align="center">
 <div class="alert alert-success">Your request has been submitted, we will be in touch shortly.</div>
+<script>
+  window.location.href = "../index.php"
+</script>
 </div>
 </p>
 </div>
